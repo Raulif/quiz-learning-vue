6b5c56f23e3f17e6303e42aa1242e217afb4d24d
raul <raul@push-founders.com>
2020-04-21 15:16:31 +0200

    added question box and basic ui

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul class="header-list">
+      <li class="header-list-item">Fancy Quiz App</li>
+      <li class="header-list-item">Counter: {{ numCorrect }}/{{ numTotal }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    numCorrect: Number,
+    numTotal: Number,
+  },
+};
+</script>
+<style scoped>
+.header-list {
+  display: flex;
+  border-bottom: 1px solid grey;
+  list-style: none;
+}
+
+.header-list-item {
+  margin-right: 1em;
+  padding: 1em;
+}
+</style>
